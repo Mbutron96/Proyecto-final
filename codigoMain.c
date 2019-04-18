f15b128062e786c9a8897c64b7d54bda5c773b54
Miguel Ángel Butrón López <34613353+Mbutron96@users.noreply.github.com>
2019-04-17 23:14:09 -0500

    Update codigoMain.c

--- codigoMain.c
+++ codigoMain.c
@@ -4,3 +4,7 @@ int main(void){
 
 return 0;
 }
+
+//La idea que tengo es que hagamos programación por capas
+//Puedes borrar todoy des hacer aquí, github tiene un control de versiones, por cualquier cosa
+//La idea es que ses éste el main del programa
